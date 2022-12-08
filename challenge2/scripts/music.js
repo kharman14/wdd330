b09fd6de7679f571d50130c63969c8f7b04f913e
kharman14 <harman.katelyn96@gmail.com>
2022-12-07 19:24:42 -0700

    Add song details to page

--- challenge2/scripts/music.js
+++ challenge2/scripts/music.js
@@ -5,6 +5,7 @@ client_access_token = 'VPR3pbs0pBcVqWjnFV7rm5HZufqZToRZNl0ob8FB1hF7NQ5Gm1rxvjG5d
 
 const search_term = document.querySelector('#keyword');
 const btn = document.querySelector('button');
+let i = 0;
 
 btn.addEventListener('click', () => {
     query = search_term.value;
@@ -31,15 +32,47 @@ const displayResults = (data) => {
     data.response.hits.forEach(hit => {
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
-        //let a = document.createElement('a');
-        // add a to h3
+
+        let div = document.createElement('div');
+        div.classList.add(`song-details${i}`);
+        let img = document.createElement('img');
+        img.setAttribute('src', hit.result.song_art_image_thumbnail_url);
+        img.setAttribute('alt', 'Song Art');
+        let div2 = document.createElement('div');
+        let h4 = document.createElement('h4');
+        let p = document.createElement('p');
+        let songReleaseDate = hit.result.release_date_for_display;
+        p.textContent = `Release Date: ${songReleaseDate}`;
+        let a = document.createElement('a');
+        let lyricURL = hit.result.stats.url;
+        a.textContent = `Genius Lyrics - ${hit.result.title} `;
+        a.setAttribute('href', lyricURL);
+
         let fullTitle = hit.result.title;
         h3.textContent = fullTitle;
-        // a.textContent = fullTitle;
-        // a.setAttribute =
-        // h3.appendChild(a);
-        article.appendChild(h3);
-        main.append(article);
-    });
+        h3.classList.add(`song${i}`);
+        h3.setAttribute('onclick', 'showSongDetails(this)');
 
+        h4.textContent = fullTitle;
+        article.appendChild(h3);
+        div.appendChild(img);
+        div2.appendChild(h4);
+        div2.appendChild(p);
+        div2.appendChild(a);
+        div.appendChild(div2);
+        article.appendChild(div);
+        main.append(article);
+
+        i++;
+    });
 }
+
+function showSongDetails(element) {
+    let current_class = element.getAttribute('class');
+    let index = current_class.charAt(current_class.length - 1);
+    if (document.querySelector(`.song-details${index}`).style.display == 'grid') {
+        document.querySelector(`.song-details${index}`).style.display = 'none';
+    } else {
+    document.querySelector(`.song-details${index}`).style.display = 'grid';
+    }
+};
