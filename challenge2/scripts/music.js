bc1dde90ae9e31054af1fd19e44f4c3a7cb38036
kharman14 <harman.katelyn96@gmail.com>
2022-12-07 20:41:26 -0700

    Make edits if input is empty or if get no results

--- challenge2/scripts/music.js
+++ challenge2/scripts/music.js
@@ -9,8 +9,10 @@ let i = 0;
 
 btn.addEventListener('click', () => {
     query = search_term.value;
-    let url = `${baseURL}?q=${query}&access_token=${client_access_token}`;
-    findSong(url);
+    if (query.length != 0) {
+        let url = `${baseURL}?q=${query}&access_token=${client_access_token}`;
+        findSong(url);
+    } 
 });
 
 async function findSong(URL) {
@@ -26,44 +28,57 @@ const displayResults = (data) => {
     let main = document.querySelector('main');
     main.innerHTML = '';
     let aside = document.createElement('aside');
-    aside.textContent = `Songs by ${query}:`;
+    let h2 = document.createElement('h2');
+    h2.textContent = `Song Results for: ${query}`;
+    aside.appendChild(h2);
     main.append(aside);
-    
-    data.response.hits.forEach(hit => {
-        let article = document.createElement('article');
+    if (data.response.hits.length === 0) {
         let h3 = document.createElement('h3');
-        let div = document.createElement('div');
-        let div2 = document.createElement('div');
-        let h4 = document.createElement('h4');
-        let p = document.createElement('p');
-        let a = document.createElement('a');
-        let img = document.createElement('img');
-
-        div.classList.add(`song-details${i}`);
-        img.setAttribute('src', hit.result.song_art_image_thumbnail_url);
-        img.setAttribute('alt', 'Song Art');
-        let songReleaseDate = hit.result.release_date_for_display;
-        p.textContent = `Release Date: ${songReleaseDate}`;
-        let lyricURL = hit.result.url;
-        a.textContent = `Genius Lyrics - ${hit.result.title} `;
-        a.setAttribute('href', lyricURL);
-        let fullTitle = hit.result.title;
-        h3.textContent = fullTitle;
-        h3.classList.add(`song${i}`);
-        h3.setAttribute('onclick', 'showSongDetails(this)');
-        h4.textContent = fullTitle;
-
+        let article = document.createElement('article');
+        h3.textContent = `Sorry, there no results for: ${query}.`
         article.appendChild(h3);
-        div.appendChild(img);
-        div2.appendChild(h4);
-        div2.appendChild(p);
-        div2.appendChild(a);
-        div.appendChild(div2);
-        article.appendChild(div);
         main.append(article);
+    } else {
+        data.response.hits.forEach(hit => {
+            let article = document.createElement('article');
+            let h3 = document.createElement('h3');
+            let div = document.createElement('div');
+            let div2 = document.createElement('div');
+            let h4 = document.createElement('h4');
+            let h5 = document.createElement('h5');
+            let p = document.createElement('p');
+            let a = document.createElement('a');
+            let img = document.createElement('img');
 
-        i++;
-    });
+            div.classList.add(`song-details${i}`);
+            img.setAttribute('src', hit.result.song_art_image_thumbnail_url);
+            img.setAttribute('alt', 'Song Art');
+            let songReleaseDate = hit.result.release_date_for_display;
+            p.textContent = `Release Date: ${songReleaseDate}`;
+            let lyricURL = hit.result.url;
+            a.textContent = `Site Link: Genius Lyrics - ${hit.result.title} `;
+            a.setAttribute('href', lyricURL);
+            let fullTitle = hit.result.title;
+            h3.textContent = fullTitle;
+            h3.classList.add(`song${i}`);
+            h3.setAttribute('onclick', 'showSongDetails(this)');
+            h4.textContent = fullTitle;
+            let artist = hit.result.artist_names;
+            h5.textContent = artist;
+
+            article.appendChild(h3);
+            div.appendChild(img);
+            div2.appendChild(h4);
+            div2.appendChild(h5);
+            div2.appendChild(p);
+            div2.appendChild(a);
+            div.appendChild(div2);
+            article.appendChild(div);
+            main.append(article);
+
+            i++;
+        });
+    }
 }
 
 function showSongDetails(element) {
